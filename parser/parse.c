8999abb88631bf0aa60fd9c7a3317155312131fe
astoria-d <astoria-d@home>
2018-02-24 11:19:40 +0900

    tree walk through parsing stage. code clean up.

--- parser/parse.c
+++ parser/parse.c
@@ -4,18 +4,27 @@
 #include "parser.h"
 #include "symbols.h"
 #include "code.h"
+#include "utlist.h"
 
 int line_num;
 
-static unsigned int ps_stage;
 static unsigned int pr_indent;
 static unsigned int pr_newline;
 static unsigned int enum_index;
 static unsigned int const_int_val;
 
 static int old_token;
-
 static char* old_identifier;
+
+struct parse_stage {
+    int     stage;
+    struct parse_stage *prev;
+    struct parse_stage *next;
+};
+static struct parse_stage* cur_stage;
+static struct parse_stage* head_stage;
+
+static void check_old_buf(void);
 
 /*bison required functions...*/
 void yyerror (char const *s) {
@@ -29,6 +38,111 @@ int yywrap (void )
 
 /*kersh implementations...*/
 
+void pre_shift_token(const char* parse_text, int token_num) {
+
+    if (token_num == '}') {
+        indent_dec(); 
+    }
+    print_token(parse_text);
+
+    switch (token_num) {
+        case IDEN:
+        /*set_last_symbol(parse_text);*/
+        check_old_buf();
+        old_identifier = strdup(parse_text);
+        break;
+
+        case TYPEDEF:
+        enter_parse_stage(token_num); 
+        break;
+
+        case ENUM:
+        enter_parse_stage(token_num); 
+        break;
+
+        case ENUM_CONSTANT:
+        cb_add_emu_def(parse_text, enum_index++);
+        break;
+
+        case DECIMAL_CONSTANT:
+        sscanf(parse_text, "%d", &const_int_val);
+        break;
+
+        case OCTAL_CONSTANT:
+        sscanf(parse_text, "%o", &const_int_val);
+        break;
+
+        case HEX_CONSTANT:
+        sscanf(parse_text, "%x", &const_int_val);
+        break;
+
+        case STRUCT:
+        case UNION:
+        enter_parse_stage(token_num); 
+        break;
+
+        case '{':
+        if (cur_stage->stage == STRUCT || cur_stage->stage == UNION) {
+            //sym_add_struct_def(old_identifier);
+            old_identifier = NULL;
+        }
+        line_break();
+        indent_inc(); 
+        break;
+
+        case ';':
+        case ':':
+        line_break();
+        break;
+
+    }
+
+    old_token = token_num;
+}
+
+/*check input token is identifier or enum constant or typedef name*/
+int check_token_type(void) {
+    /*printf("ps_stage: %d\n", ps_stage);*/
+    if (cur_stage->stage == ENUM && (old_token == '{' || old_token == ',') ) {
+        return ENUM_CONSTANT;
+    }
+    else {
+        return IDEN;
+    }
+}
+
+void enter_parse_stage(int stage) {
+    struct parse_stage* ps;
+
+    if (stage == ENUM) {
+        enum_index = 0;
+    }
+    ps = malloc(sizeof(struct parse_stage));
+    memset(ps, 0, sizeof(struct parse_stage));
+    ps->stage = stage;
+    DL_APPEND(head_stage, ps);
+    cur_stage = ps;
+    //printf("stage %d entered, %08x\n", stage, ps);
+}
+
+void exit_parse_stage(void) {
+    struct parse_stage* ps;
+
+    ps = cur_stage;
+    cur_stage = cur_stage->prev;
+    DL_DELETE(head_stage, ps);
+    //printf("stage %d exite, %08x\n", ps->stage, ps);
+    free(ps);
+}
+
+int get_const_val(void) {
+    return const_int_val;
+}
+
+void set_enum_index(int next_index) {
+    enum_index = next_index;
+}
+
 void free_identifer(void) {
     free(old_identifier);
     old_identifier = NULL;
@@ -40,90 +154,6 @@ static void check_old_buf(void) {
         printf(">>[%s] symbol left unfree...", old_identifier);
         free_identifer();
     }
-}
-
-void init_parser(void) {
-    extern int yydebug;
-    yydebug = 0;
-    line_num = 1;
-    ps_stage = 0;
-    pr_indent = 0;
-    pr_newline = 0;
-    old_identifier = NULL;
-    init_code_block();
-}
-
-void exit_parser(void) {
-    check_old_buf();
-    exit_code_block();
-}
-
-void pre_shift_token(const char* parse_text, int token_num) {
-    print_token(parse_text);
-    old_token = token_num;
-    if (token_num == IDEN) {
-        /*set_last_symbol(parse_text);*/
-        check_old_buf();
-        old_identifier = strdup(parse_text);
-    }
-    else if (token_num == TYPEDEF) {
-        enter_parse_stage(TYPEDEF); 
-    }
-    else if (token_num == ENUM) {
-        enter_parse_stage(ENUM); 
-    }
-    else if (token_num == ENUM_CONSTANT) {
-        cb_add_emu_def(parse_text, enum_index++);
-    }
-    else if (token_num == DECIMAL_CONSTANT) {
-        sscanf(parse_text, "%d", &const_int_val);
-    }
-    else if (token_num == OCTAL_CONSTANT) {
-        sscanf(parse_text, "%o", &const_int_val);
-    }
-    else if (token_num == HEX_CONSTANT) {
-        sscanf(parse_text, "%x", &const_int_val);
-    }
-    else if (token_num == STRUCT) {
-        enter_parse_stage(STRUCT); 
-    }
-    else if (token_num == '{') {
-        if (ps_stage == STRUCT) {
-            //sym_add_struct_def(old_identifier);
-            old_identifier = NULL;
-        }
-    }
-}
-
-int get_const_val(void) {
-    return const_int_val;
-}
-
-void set_enum_index(int next_index) {
-    enum_index = next_index;
-}
-
-int check_token_type(void) {
-    /*printf("ps_stage: %d\n", ps_stage);*/
-    if (ps_stage == ENUM && (old_token == '{' || old_token == ',') ) {
-        return ENUM_CONSTANT;
-    }
-    else {
-        return IDEN;
-    }
-}
-
-void enter_parse_stage(int stage) {
-    if (stage == ENUM) {
-        enum_index = 0;
-    }
-    /*printf("stage %d entered\n", stage);*/
-    ps_stage = stage;
-}
-
-void exit_parse_stage(void) {
-    /*if (ps_stage != 0) printf("stage %d exited ", ps_stage);*/
-    ps_stage = 0;
 }
 
 void line_break(void) {
@@ -148,4 +178,24 @@ void indent_inc(void) {
 
 void indent_dec(void) {
     pr_indent--;
+}
+
+void init_parser(void) {
+    extern int yydebug;
+    yydebug = 0;
+    line_num = 1;
+    cur_stage = NULL;
+    head_stage = NULL;
+    pr_indent = 0;
+    pr_newline = 0;
+    old_identifier = NULL;
+    init_code_block();
+}
+
+void exit_parser(void) {
+    if (head_stage) {
+        free(head_stage);
+    }
+    check_old_buf();
+    exit_code_block();
 }
